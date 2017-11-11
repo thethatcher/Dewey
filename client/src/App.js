edd8f73e28985bce5f92345701f79d10b9f423f9
Ryan Thatcher <thethatcher@gmail.com>
2017-11-10 19:30:35 -0700

    heroku testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Stories from "./pages/Stories";
 import Nav from "./components/Nav";
 
 const App = () => 
   <div>
     <Nav />
-    <Stories />
   </div>;
 
 export default App;
