95d06fc8554aac4fe7f0b616dc9c1c477e5c3feb
MICHAEL HEDIN <MICHAEL HEDIN>
2017-11-11 10:41:56 -0700

    updates

---
+++ routes/api/categories.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const categoriesController = require("../../controllers/categoriesController");
+
+// Matches with "/api/categories"
+router.route("/")
+  .get(categoriesController.findAll)
+  .post(categoriesController.create);
+
+// Matches with "/api/categories/:id"
+router
+  .route("/:id")
+  .get(categoriesController.findById)
+  .put(categoriesController.update)
+  .delete(categoriesController.remove);
+
+module.exports = router;
