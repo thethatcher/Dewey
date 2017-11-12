f6ec2b706b932beaa67253254090d38e724520b8
Ryan Thatcher <thethatcher@gmail.com>
2017-11-11 20:16:45 -0700

    api/users routes are functioning properly

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,39 @@
 const router = require("express").Router();
 const db = require("../../models");
 
+//matches requestes to /api/users
 router.route("/")
 	.get((req,res)=>{
-		db.User.find({})
-			.then((dbResults) =>{
-				res.json(dbResults);
-			});
+		db.User.findAll({})
+		.then((dbResults) =>{
+			res.json(dbResults);
+		});
+	})
+	.post((req, res) => {
+		db.User.findOrCreate({
+			where:{
+				username: req.body.username
+			},
+			defaults:{nickname: req.body.nickname}
+		}).then((dbResults) =>{
+			//if this is true a new user was created. 
+			if (dbResults[1]) {
+				res.json(dbResults[0]);
+			}
+			//if false the username is taken.
+			else{
+				res.json({message:"Username already exists", user: dbResults[0]});
+			}
+		});
 	});
 
+//matches requests to /api/users/:id
+router.route("/:id")
+	.get((req,res) =>{
+		db.User.find({
+			where:{id: req.params.id}
+		}).then((dbResults)=>{
+			res.json(dbResults);
+		})
+	});
 module.exports = router;
