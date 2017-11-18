3204b3b6ed9b4c7c325b8f145c3a538e53335fff
Ryan Thatcher <thethatcher@gmail.com>
2017-11-18 11:00:30 -0700

    finished category delete route

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -39,7 +39,21 @@ router.route("/:id")
 	})
 })
 .delete((req,res)=>{
-	res.json({});
+	//disassociate the user with the category. do not delete it completely. 
+	db.Category.findOne({where: {id:req.params.id}})
+	.then((dbResult)=>{
+		console.log(dbResult);
+		db.User.findOne({where:{id:2}}) //DELTA update 2 to be the dynamic userID.
+		.then((dbUser)=>{ 
+			dbResult.removeUser(dbUser)
+			.then((dbResult)=>{res.json(dbResult)});
+		});
+		//delete all items and transactions assocaited to that category.
+		db.Item.destroy({
+			where:{UserId: 2, CategoryId: dbResult.id} //DELTA
+		})
+		.then((destroyResult)=>{res.json(destroyResult);});
+	}); 
 });
 
 router.route("/safe")
