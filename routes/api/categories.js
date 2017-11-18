df230410afa9398913586c70b6e13abb38e4d131
Ryan Thatcher <thethatcher@gmail.com>
2017-11-18 10:35:31 -0700

    adding category routes to get/update/delete specific categories for users

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -10,14 +10,36 @@ router.route("/")
 })
 .post((req,res)=>{
 	//looks for an existing category with the name input by user. creates a new category if an existing one is not found. 
+		//adds an association between the returned Category and the user specified to the user_category_junction table.
+	db.Category.findOrCreate({
+		where:{name: req.body.name}
+		,defaults:{safe: null}
+	}).then((dbResult)=>{
+		dbResult[0].addUser(2); //TODO DELTA replace the '2' with the currently logged in user's ID
+		res.json(dbResult);
+	});
+});
+
+router.route("/:id")
+.get((req, res)=>{
+	db.Category.find({
+		where: {id: req.params.id}
+	}).then((dbResult)=>{
+		res.json(dbResult);
+	})
+})
+.put((req, res)=>{
 	db.Category.findOrCreate({
 		where:{name: req.body.name}
 		,defaults:{safe: null}
 	}).then((dbResult)=>{
 		//adds an association between the returned Category and the user specified to the user_category_junction table.
-		dbResult[0].addUser(2); //TODO replace the '2' with the currently logged in user's ID
+		dbResult[0].addUser(2); //TODO DELTA replace the '2' with the currently logged in user's ID
 		res.json(dbResult);
-	});
+	})
+})
+.delete((req,res)=>{
+	res.json({});
 });
 
 router.route("/safe")
